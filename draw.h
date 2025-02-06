4c10fa329a534c414ab42e6a0f32effdc37f1f47
Jack Punches <jackpunches@Jacks-MacBook-Pro-3.local>
2025-02-06 11:06:20 -0700

    added print char 8x12 and print arrow

--- draw.h
+++ draw.h
@@ -7,4 +7,10 @@ void print_char_5x7(char c);
 
 extern const unsigned char font_5x7[][5];
 
+void print_char_8x12(char c);
+
+extern const unsigned char font_8x12[][12];
+
+void print_arrow(int leftCol, int size);
+
 #endif
